23efa005ab18917a2b76f3870caf8f52ade2ba4e
Rahul Singh <learn.rahulsingh2@gmail.com>
2021-05-16 23:40:12 +0530

    flyway gradle

---
+++ src/main/java/io/rsi/codespace/springgradleflyway/SpringGradleFlywayApplication.java
@@ -0,0 +1,13 @@
+package io.rsi.codespace.springgradleflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGradleFlywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGradleFlywayApplication.class, args);
+    }
+
+}
